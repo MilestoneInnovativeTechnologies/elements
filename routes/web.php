14b269b5669fbecf848c7c1cd04f216780725920
sona-viswam <sona.viswam@gmail.com>
2022-09-06 15:30:59 +0530

    check

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ Route::get('/', function () {
 });
 Route::get('itemlist', [ItemController::class, 'itemlist'])->name('itemlist');
 Route::get('customerlist', [CustomersController::class, 'customerlist'])->name('customerlist');
+Route::get('customerdetails', [CustomersController::class, 'customerdetails'])->name('customerdetails');
 Route::get('ordersummary', [OrderController::class, 'ordersummary'])->name('ordersummary');
 Route::get('profile', [UserController::class, 'profile'])->name('profile');
