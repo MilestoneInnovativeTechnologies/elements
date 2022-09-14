789a70a2661adf3fd7e42160269b0801dd3d82cf
Anju KV <anju@milestoneit.net>
2022-09-14 16:56:50 +0530

    ordersummary

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -19,7 +19,7 @@ class OrderController extends Controller
             'reference_number' => 'required|max:20',
             'sales_executive' => 'required|max:20',
             'status'=>'required',
-            'foctax'=>'required', 'in:Yes,No',
+            'foctax'=>'required',
 
             'credit_period'=>'required',
 
@@ -34,18 +34,16 @@ class OrderController extends Controller
 
 
         ]);
-        $orders=new Order();
-        $orders->id=$request->id;
-        $orders->order_date=$request->order_date;
-        $orders->sales_executive=$request->sales_executive;
-        $orders->reference_number=$request->reference_number;
-        $orders->status=$request->status;
-        $orders->foctax=$request->foctax;
-        $orders->credit_period=$request->credit_period;
-        $orders->save();
-
-
-
+        $order=new Order();
+        $order->id=$request->id;
+        $order->order_date=$request->order_date;
+        $order->sales_executive=$request->sales_executive;
+        $order->reference_number=$request->reference_number;
+        $order->status=$request->status;
+        $order->foctax=$request->foctax;
+        $order->credit_period=$request->credit_period;
+        $order->save();
+        return redirect('/orderhistory');
 //        return back()->with('success','Successfully place order');
     }
 }
--- src/Models/Order.php
+++ src/Models/Order.php
@@ -10,6 +10,17 @@ class Order extends Model
     use HasFactory;
 
     protected $table = 'orders';
+    protected $fillable=[
+        'id',
+        'order_date',
+        'sales_executive',
+        'reference_number',
+        'payment_mode',
+        'credir_period',
+        'foctax',
+        'invoice_discount',
+        'status',
+    ];
 
 
     protected $guard = [];
