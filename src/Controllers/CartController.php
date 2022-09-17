bbf7792239ab1d97ad60be9584f596bb945827fd
sona-viswam <sona.viswam@gmail.com>
2022-09-17 17:01:13 +0530

    cart with error

--- src/Controllers/CartController.php
+++ src/Controllers/CartController.php
@@ -3,29 +3,54 @@
 namespace Milestone\Elements\Controllers;
 
 use Illuminate\Http\Request;
+use Milestone\Elements\Models\Customers;
 use Milestone\Elements\Models\Item;
+
 
 class CartController extends Controller
 {
+    public function selectcustomer(Request $request)
+    {
+        if ($request->has('customerId')) {
+            $customerId = $request->input('customerId');
+            $customer = Customers::where('id', $customerId)->get();
+//            $customername = $customer[0]->display_name;
+            $customername = 'TestName';
+            $request->session()->put('customerId', $customerId);
+            $request->session()->put('customername', $customername);
+            return redirect()->back()->with('success', 'You have selected Customer successfully!'.$request->session()->get('customername'));
+        }
+    }
+
     public function addtocart(Request $request)
     {
-        if($request->has('myId')) {
+        $cart = "";
+        if ($request->has('myId')) {
             $id = $request->input('myId');
-//            dd($id);
-            $cart = session()->get('cart', []);
-            if(isset($cart[$id])) {
+            if (!$request->session()->has('cart')) {
+                $cart = $request->session()->put('cart', []);
+            } else {
+                $cart = $request->session()->get('cart');
+            }
+            print_r($cart);
+            if (isset($cart[$id])) {
                 $cart[$id]['quantity']++;
             } else {
                 $item = Item::where('id', $id)->get();
                 $cart[$id] = [
                     "name" => $item[0]->displayname,
                     "quantity" => 1,
-                    "rate" =>  $item[0]->rate,
+                    "rate" => $item[0]->rate,
                 ];
             }
-            session()->put('cart', $cart);
+            print_r($cart);
+            $request->session()->put('cart', $cart);
+            dd($request->session()->get('cart'));
             return redirect()->back()->with('success', 'Product added to cart successfully!');
         }
-//        https://laraveltuts.com/laravel-9-shopping-cart-tutorial-with-ajax-example/
     }
+
+
+//        https://laraveltuts.com/laravel-9-shopping-cart-tutorial-with-ajax-example/
+
 }
