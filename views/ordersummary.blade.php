5f965b559ea49e28c1f0a3ea379fe5ff38a12659
sona-viswam <sona.viswam@gmail.com>
2022-09-24 14:55:49 +0530

    edit popup  added

--- views/ordersummary.blade.php
+++ views/ordersummary.blade.php
@@ -121,9 +121,6 @@
 
                                 <br>
                                 <br>
-                                <br>
-                                <br>
-
                             <div class="card mb-4">
                             <div class="card-body">
                                 <h5 class="card-title">Order Details</h5>
@@ -169,60 +166,61 @@
                                                 <td>{{$taxtamount}}</td>
                                                 <td>{{$totalamount}}</td>
                                                 <td>
+
+
+                                                    <!-- Edit Modal -->
+                                                    <div class="modal fade" id="smallModal" tabindex="-1" aria-hidden="true">
+                                                        <div class="modal-dialog modal-sm" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLabel2">Edit</h5>
+                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <div class="row">
+                                                                        <div class="col mb-3">
+                                                                            <label for="nameSmall" class="form-label">Name</label>
+                                                                            <input type="text" id="nameSmall" class="form-control" placeholder="Enter Name" disabled>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="row g-2">
+                                                                        <div class="col mb-0">
+                                                                            <label class="form-label" for="emailSmall">Quantity</label>
+                                                                            <input type="number" class="form-control" id="emailSmall">
+                                                                        </div>
+                                                                        <div class="col mb-0">
+                                                                            <label for="dobSmall" class="form-label">FOC Quantity</label>
+                                                                            <input id="dobSmall" type="number" class="form-control">
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                                                                        Close
+                                                                    </button>
+                                                                    <button type="button" class="btn btn-primary">Save Changes</button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- Edit Modal Ends -->
+
+
                                                     <div class="dropdown">
                                                         <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
                                                             <i class="bx bx-dots-vertical-rounded"></i>
                                                         </button>
                                                         <div class="dropdown-menu">
-                                                            <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
-                                                            <!-- Edit Modal -->
-                                                            <div class="modal fade" id="smallModal" tabindex="-1" aria-hidden="true">
-                                                                <div class="modal-dialog modal-sm" role="document">
-                                                                    <div class="modal-content">
-                                                                        <div class="modal-header">
-                                                                            <h5 class="modal-title" id="exampleModalLabel2">Modal title</h5>
-                                                                            <button
-                                                                                type="button"
-                                                                                class="btn-close"
-                                                                                data-bs-dismiss="modal"
-                                                                                aria-label="Close"
-                                                                            ></button>
-                                                                        </div>
-                                                                        <div class="modal-body">
-                                                                            <div class="row">
-                                                                                <div class="col mb-3">
-                                                                                    <label for="nameSmall" class="form-label">Name</label>
-                                                                                    <input type="text" id="nameSmall" class="form-control" placeholder="Enter Name" />
-                                                                                </div>
-                                                                            </div>
-                                                                            <div class="row g-2">
-                                                                                <div class="col mb-0">
-                                                                                    <label class="form-label" for="emailSmall">Email</label>
-                                                                                    <input type="text" class="form-control" id="emailSmall" placeholder="xxxx@xxx.xx" />
-                                                                                </div>
-                                                                                <div class="col mb-0">
-                                                                                    <label for="dobSmall" class="form-label">DOB</label>
-                                                                                    <input id="dobSmall" type="text" class="form-control" placeholder="DD / MM / YY" />
-                                                                                </div>
-                                                                            </div>
-                                                                        </div>
-                                                                        <div class="modal-footer">
-                                                                            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                                                                                Close
-                                                                            </button>
-                                                                            <button type="button" class="btn btn-primary">Save changes</button>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                            <!-- Edit Modal Ends -->
+                                                            <a class="dropdown-item" href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#smallModal">
+                                                                <i class="bx bx-edit-alt me-1"></i> Edit</a>
+
                                                             <a class="dropdown-item" href="javascript:void(0);"
                                                                data-bs-toggle="modal"
-                                                               data-bs-target="#modalToggle"><i class="bx bx-trash me-1"></i> Delete</a>
+                                                               data-bs-target="#deleteModal"><i class="bx bx-trash me-1"></i> Delete</a>
                                                         </div>
                                                         <!--Delete Modal -->
                                                         <div class="modal fade"
-                                                            id="modalToggle"
+                                                            id="deleteModal"
                                                             aria-labelledby="modalToggleLabel"
                                                             tabindex="-1"
                                                             style="display: none"
