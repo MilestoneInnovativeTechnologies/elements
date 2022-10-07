22730be888496433576efc7a94298aeda3f8e3bd
Anju KV <anju@milestoneit.net>
2022-10-07 10:09:31 +0530

    completed ordersummary

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -17,9 +17,6 @@ class OrderController extends Controller
         $request->validate( [
             'payment_mode' => 'required|in:cash,credit',
             'reference_number' => 'required|max:20',
-            'sales_executive' => 'required|max:20',
-            'status'=>'required',
-            'foctax'=>'required',
             'credit_period'=>'required_if:payment_mode,credit',
         ]);
 //        if (payment_mode == credit) {
@@ -29,11 +26,12 @@ class OrderController extends Controller
 //        }
 
         $order=new Order();
-        $order->id=$request->id;
+//        $order->id=$request->id;
         $order->order_date=$request->order_date;
         $order->customer=session('customerId');
         $order->reference_number=$request->reference_number;
         $order->invoice_discount=$request->invoice_discount;
+        $order->narration=$request->narration;
         $order->credit_period=$request->credit_period;
 
         if($request->foctaxcheck=='on')
@@ -47,7 +45,9 @@ class OrderController extends Controller
         $order->foctax=$foc;
         $order->sales_executive=1;
         $order->save();
+        $request->session()->flash('status', 'Task was successful!');
         $request->session()->flush();
+
         return redirect('index');
 
     }
