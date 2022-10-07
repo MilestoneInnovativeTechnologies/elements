72a3a3e5ecb83db21ecf3704bf1572f84228c9fd
sona-viswam <sona.viswam@gmail.com>
2022-10-07 11:04:49 +0530

    web.php updated

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,19 @@ Route::get('/', function () {
     return view('Elements::se_dashboard');
 });
 Route::get('index', [SalesexecutiveController::class, 'index'])->name('index');
+
 Route::get('customerlist', [CustomersController::class, 'customerlist'])->name('customerlist');
-Route::get('itemlist', [ItemController::class, 'itemlist'])->name('itemlist');
 Route::get('searchcustomer', [CustomersController::class, 'searchcustomer'])->name('searchcustomer');
 Route::get('selectcustomer', [CustomersController::class, 'selectcustomer'])->name('selectcustomer');
-Route::get('searchitems', [ItemController::class, 'searchitems'])->name('searchitems');
-Route::get('addtocart', [CartController::class, 'addtocart'])->name('addtocart');
-
 Route::get('customerdetails', [CustomersController::class, 'customerdetails'])->name('customerdetails');
-Route::get('ordersummary', [OrderController::class, 'ordersummary'])->name('ordersummary');
 
+Route::get('itemlist', [ItemController::class, 'itemlist'])->name('itemlist');
+Route::get('searchitems', [ItemController::class, 'searchitems'])->name('searchitems');
+
+Route::get('ordersummary', [OrderController::class, 'ordersummary'])->name('ordersummary');
 Route::post('saveorder', [OrderController::class, 'saveorder'])->name('saveorder');
+
+Route::get('addtocart', [CartController::class, 'addtocart'])->name('addtocart');
 Route::post('deleteitem', [CartController::class, 'deleteitem'])->name('deleteitem');
 Route::post('updateitem', [CartController::class, 'updateitem'])->name('updateitem');
 Route::post('invoicediscount', [CartController::class, 'invoicediscount'])->name('invoicediscount');
